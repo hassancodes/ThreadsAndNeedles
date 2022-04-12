549cbb7d10e7032767a59d02bb7caac0adabed6e
Muhammad hassan <hassanhanjra900@gmail.com>
2022-04-11 22:55:01 -0400

    feat:basic features done

---
+++ JS/action.js
@@ -0,0 +1,14 @@
+const myPreloader = document.querySelector('.preloader');
+
+const fadeOutEffect = setInterval(() => {
+    if (!preloader.style.opacity) {
+      preloader.style.opacity = 1;
+    }
+    if (preloader.style.opacity > 0) {
+      preloader.style.opacity -= 0.1;
+    } else {
+      clearInterval(fadeEffect);
+    }
+  }, 300);
+
+  window.addEventListener('load', fadeOutEffect);
